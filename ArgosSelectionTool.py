2c9b43b04819c2deeba9e24d68760f48798f1061
lilym572 <lily.mesnik@duke.edu>
2026-09-23 12:20:43 -0400

    seeing if variables are in box

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -15,8 +15,7 @@ the_box = {
     'x_min' : 34.00,
     'y_min' : -76.00,
     'x_max' : 34.50,
-    'y_max' : -75.00
-}
+    'y_max' : -75.00}
 # Copy and paste a line of data as the lineString variable value
 lineString = '10154641232,true,2019-05-14 13:37:52.000,-75.49356999999998,34.86216,,0.0,-127.0,4.0167976787E8,5141.0,424,"40",34.86216,34.86216,"0",-75.49356999999998,-75.49356999999998,6,0,3,61.0,381.0,10718.0,2466.0,150,187,2,0,"1",,,"argos-doppler-shift","Pterodroma hasitata","174441","HA09","Satellite tracking of black-capped petrels, 2019"'
     
@@ -26,10 +25,17 @@ line_data = lineString.split(",")
 # Assign variables to specfic items in the list
 event_id = line_data[0]   # Argos tracking event ID ("event-id")
 timestamp = line_data[2]  # Observation date ("timestamp")
-lat = line_data[4]        # Observation latitude  ("location-lat")
-lon = line_data[3]        # Observation longitude ("location-lon")
+lat = float(line_data[4])        # Observation latitude  ("location-lat")
+lon = float(line_data[3])        # Observation longitude ("location-lon")
 lc  = line_data[14]        # Observation location class ("argos:lc")
 tag_id = line_data[34]     # Tag identifier ("tag-local-identifier")
   
 # Print information to the use
-print (f"Record {event_id} indicates {tag_id} was seen at {lat}N and {lon}W on {timestamp}")
+#print (f"Record {event_id} indicates {tag_id} was seen at {lat}N and {lon}W on {timestamp}")
+#Evaluate latitude and longitude conditions
+lat_condition = the_box['y_min'] < lat < the_box['y_max']
+lon_condition = the_box['x_min'] < lon < the_box['x_max']
+if lat_condition & lon_condition:
+    print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
+else:
+    print(f'Record {event_id}: {tag_id} was NOT IN the box at {timestamp}')
